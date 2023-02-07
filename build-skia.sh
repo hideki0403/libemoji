29667770673b93c057987b9c420182979a2985a5
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-02-07 16:36:28 +0900

    fix

--- build-skia.sh
+++ build-skia.sh
@@ -2,9 +2,14 @@
 
 gn_path=$(dirname ${0})/externals/depot_tools/gn
 ninja_path=$(dirname ${0})externals/depot_tools/ninja
-args=$(echo "$1" | sed -e 's/\\//g')
 
-echo build args: $1
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  args='is_debug=false target_cpu="x64" is_official_build=true skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_icu=false skia_use_system_harfbuzz=false skia_use_system_expat=false skia_use_system_zlib=false extra_cflags=["-MTd"]'
+else
+  args="is_debug=false target_cpu=\"\"x64\"\" is_official_build=true skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_icu=false skia_use_system_harfbuzz=false skia_use_system_expat=false skia_use_system_zlib=false cc=\"\"clang\"\" cxx=\"\"clang++\"\" clang_win=\"\"%LLVM_PATH%\"\" win_vc=\"\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\VC\"\" extra_cflags=[\"\"-MTd\"\"]"
+fi
+
+echo build args: $args
 
 python3 tools/git-sync-deps
 $gn_path gen out/Static --args=$1
