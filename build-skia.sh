2e7604a43345871520265dee081e4e77b5ee0b2f
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-02-10 01:02:56 +0900

    remove symbol_level arg

--- build-skia.sh
+++ build-skia.sh
@@ -6,9 +6,9 @@ ninja_path=$(dirname ${0})/externals/depot_tools/ninja.py
 useTempDir=false
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  args='is_debug=false target_cpu="x64" is_official_build=true symbol_level=0 extra_cflags=["-MT"]'
+  args='is_debug=false target_cpu="x64" is_official_build=true skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_icu=false skia_use_system_harfbuzz=false skia_use_system_expat=false skia_use_system_zlib=false extra_cflags=["-MT"]'
 else
-  args="is_debug=false target_cpu=\"\"x64\"\" is_official_build=true skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_icu=false skia_use_system_harfbuzz=false skia_use_system_expat=false skia_use_system_zlib=false symbol_level=0 cc=\"\"clang\"\" cxx=\"\"clang++\"\" clang_win=\"\"%LLVM_PATH%\"\" win_vc=\"\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\VC\"\" extra_cflags=[\"\"-MT\"\"]"
+  args="is_debug=false target_cpu=\"\"x64\"\" is_official_build=true skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_icu=false skia_use_system_harfbuzz=false skia_use_system_expat=false skia_use_system_zlib=false cc=\"\"clang\"\" cxx=\"\"clang++\"\" clang_win=\"\"%LLVM_PATH%\"\" win_vc=\"\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\VC\"\" extra_cflags=[\"\"-MT\"\"]"
   useTempDir=true
 fi
 
