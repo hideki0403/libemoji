3b5a6c4ad263f2b5838dfb86502b0c5647c8a95a
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-08-13 19:18:44 +0900

    update

--- build-skia.sh
+++ build-skia.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+if [[ "$SKIP_SKIA_BUILD" == "true" ]]; then
+  echo "Skip skia build"
+  exit
+fi
+
 gn_path=$(dirname ${0})/externals/depot_tools/gn.py
 ninja_path=$(dirname ${0})/externals/depot_tools/ninja.py
 
@@ -7,11 +12,6 @@ if [[ "$OSTYPE" == "linux-gnu"* ]]; then
   args='is_debug=false target_cpu="x64" is_official_build=false'
 else
   args="is_debug=false target_cpu=\"\"x64\"\" is_official_build=false cc=\"\"clang\"\" cxx=\"\"clang++\"\" clang_win=\"\"%LLVM_PATH%\"\" win_vc=\"\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\VC\"\""
-fi
-
-if [[ "$SKIP_SKIA_BUILD" == "true" ]]; then
-  echo "Skip skia build"
-  exit
 fi
 
 echo build args: $args
