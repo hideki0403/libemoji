cac31edcce42f66b6686e5f0bde200cc31607705
yukineko <27853966+hideki0403@users.noreply.github.com>
2023-08-16 11:12:20 +0900

    update

--- build-skia.sh
+++ build-skia.sh
@@ -9,9 +9,9 @@ gn_path=$(dirname ${0})/externals/depot_tools/gn.py
 ninja_path=$(dirname ${0})/externals/depot_tools/ninja.py
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-  args='is_debug=false target_cpu="x64" is_official_build=false'
+  args='is_debug=false target_cpu="x64" is_official_build=true skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_icu=false'
 else
-  args="is_debug=false target_cpu=\"\"x64\"\" is_official_build=false cc=\"\"clang\"\" cxx=\"\"clang++\"\" clang_win=\"\"%LLVM_PATH%\"\" win_vc=\"\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\VC\"\""
+  args="is_debug=false target_cpu=\"\"x64\"\" is_official_build=true skia_use_system_libjpeg_turbo=false skia_use_system_libpng=false skia_use_system_libwebp=false skia_use_system_icu=false cc=\"\"clang\"\" cxx=\"\"clang++\"\" clang_win=\"\"%LLVM_PATH%\"\" win_vc=\"\"C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\Enterprise\\VC\"\""
 fi
 
 echo build args: $args
